6b6a8e7c42d0eba1ed84567e52f34ee95cae0037
Javier Balsas <javier9518@gmail.com>
2020-05-08 23:30:17 -0500

    provider products

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_app/screens/product_details_Screen.dart';
 import './screens/products_overview_screen.dart';
+import './providers/products.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primaryColor: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato'
+    return ChangeNotifierProvider(
+      create: (ctx)=>Products(),
+        child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primaryColor: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato'
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routedNamed:(ctx)=>ProductDetailScreen()
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routedNamed:(ctx)=>ProductOverviewScreen()
-      },
     );
   }
 }
--- shop_app/lib/widget/product_item.dart
+++ shop_app/lib/widget/product_item.dart
@@ -1,36 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../screens/product_details_Screen.dart';
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-final String id;
-final String title;
-final String imageUrl;
+// final String id;
+// final String title;
+// final String imageUrl;
 
-  const ProductItem(this.id, this.title, this.imageUrl);
+//   const ProductItem(this.id, this.title, this.imageUrl);
+
+
   @override
   Widget build(BuildContext context) {
+ final product= Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
           child: GridTile(
         child: Image.network(
-          imageUrl,
+          product.imageUrl,
           fit: BoxFit.cover,
         ),
         footer: GestureDetector(
           onTap: (){
-            Navigator.of(context).pushNamed(ProductDetailScreen.routedNamed, arguments:{
-              'title':title
-            });
+            Navigator.of(context).pushNamed(ProductDetailScreen.routedNamed, arguments:product.id);
           },
                   child: GridTileBar(
             backgroundColor: Colors.black87,
             leading: IconButton(
-              icon: Icon(Icons.favorite),
-              onPressed: (){},
+              icon: Icon(product.isFavorite? Icons.favorite:Icons.favorite_border),
+              onPressed: (){
+                product.toggleFavoriteStatus();
+              },
                color: Theme.of(context).accentColor,
             ),
             title: Text(
-              title, 
+              product.title, 
             textAlign: TextAlign.center,
             ), 
             trailing: IconButton(
