a1d4fa2153b7498c6972353d747925754ebd645d
Javier Balsas <javier9518@gmail.com>
2020-05-19 23:10:34 -0500

    tienda app new login

--- tienda_app/lib/screens/auth_screen.dart
+++ tienda_app/lib/screens/auth_screen.dart
@@ -18,68 +18,78 @@ class AuthScreen extends StatelessWidget {
     // transformConfig.translate(-10.0);
     return Scaffold(
       // resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
-                  Color.fromRGBO(255, 188, 117, 1).withOpacity(0.9),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.center,
+            colors: [
+              Color.fromRGBO(17, 95, 188, 1).withOpacity(0.9),
+              Color.fromRGBO(100, 170, 237, 1).withOpacity(0.5),
+            ],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Login",
+                      style: TextStyle(color: Colors.white, fontSize: 40)),
+                  Text(
+                    "Welcome Back",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  )
                 ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: [0, 1],
               ),
             ),
-          ),
-          SingleChildScrollView(
-            child: Container(
-              height: deviceSize.height,
-              width: deviceSize.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Flexible(
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 20.0),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
-                      transform: Matrix4.rotationZ(-8 * pi / 180)
-                        ..translate(-10.0),
-                      // ..translate(-10.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.deepOrange.shade900,
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 8,
-                            color: Colors.black26,
-                            offset: Offset(0, 2),
-                          )
-                        ],
-                      ),
-                      child: Text(
-                        'MyShop',
-                        style: TextStyle(
-                          color: Theme.of(context).accentTextTheme.title.color,
-                          fontSize: 50,
-                          fontFamily: 'Anton',
-                          fontWeight: FontWeight.normal,
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60))),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 60,
                         ),
-                      ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10),
+                                  color: Color.fromRGBO(32, 132, 232, 0.3),
+                                )
+                              ]),
+                          child: Column(
+                            children: [AuthCard()],
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  Flexible(
-                    flex: deviceSize.width > 600 ? 2 : 1,
-                    child: AuthCard(),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -224,108 +234,153 @@ class _AuthCardState extends State<AuthCard>
 
   @override
   Widget build(BuildContext context) {
-    final deviceSize = MediaQuery.of(context).size;
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      elevation: 8.0,
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeIn,
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        // height: _heightAnimation.value.height,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'E-Mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value.isEmpty || !value.contains('@')) {
-                      return 'Invalid email!';
-                    }
-                  },
-                  onSaved: (value) {
-                    _authData['email'] = value;
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  controller: _passwordController,
-                  validator: (value) {
-                    if (value.isEmpty || value.length < 5) {
-                      return 'Password is too short!';
-                    }
-                  },
-                  onSaved: (value) {
-                    _authData['password'] = value;
-                  },
-                ),
-                AnimatedContainer(
-                  constraints: BoxConstraints(
-                    minHeight: _authMode == AuthMode.Signup ? 60 : 0,
-                    maxHeight: _authMode == AuthMode.Signup ? 120 : 0,
-                  ),
-                  duration: Duration(milliseconds: 300),
-                  curve: Curves.easeIn,
-                  child: FadeTransition(
-                    opacity: _opacityAnimation,
-                    child: SlideTransition(
-                      position: _slideAnimation,
-                      child: TextFormField(
-                        enabled: _authMode == AuthMode.Signup,
-                        decoration:
-                            InputDecoration(labelText: 'Confirm Password'),
-                        obscureText: true,
-                        validator: _authMode == AuthMode.Signup
-                            ? (value) {
-                                if (value != _passwordController.text) {
-                                  return 'Passwords do not match!';
-                                }
-                              }
-                            : null,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                if (_isLoading)
-                  CircularProgressIndicator()
-                else
-                  RaisedButton(
-                    child:
-                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
-                    onPressed: _submit,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Theme.of(context).primaryTextTheme.button.color,
-                  ),
-                FlatButton(
-                  child: Text(
-                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
-                  onPressed: _switchAuthMode,
-                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  textColor: Theme.of(context).primaryColor,
-                ),
-              ],
+    // final deviceSize = MediaQuery.of(context).size;
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeIn,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(60), topRight: Radius.circular(60))),
+      // height: _authMode == AuthMode.Signup ? 320 : 260,
+      // height: _heightAnimation.value.height,
+      // constraints:
+      // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+      // width: deviceSize.width * 0.75,
+      padding: EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            _TextfiedBuild(
+              authData: _authData,
+              icono: Icons.alternate_email,
+              labeltext: "Email",
+              textInputType: TextInputType.emailAddress,
+              type: 'email',
+              validator: (value) {
+                if (value.isEmpty || !value.contains('@')) {
+                  return 'Invalid email!';
+                }
+              },
             ),
-          ),
+            _TextfiedBuild(
+              authData: _authData,
+              icono: Icons.lock,
+              labeltext: "Password",
+              type: 'password',
+              validator: (value) {
+                if (value.isEmpty || value.length < 5) {
+                  return 'Password is too short!';
+                }
+              },
+              textEditingController: _passwordController,
+              obscuretext: true,
+            ),
+            AnimatedContainer(
+              constraints: BoxConstraints(
+                minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                maxHeight: _authMode == AuthMode.Signup ? 120 : 0,
+              ),
+              duration: Duration(milliseconds: 300),
+              curve: Curves.easeIn,
+              child: FadeTransition(
+                opacity: _opacityAnimation,
+                child: SlideTransition(
+                  position: _slideAnimation,
+                  child: _TextfiedBuild(
+                    authData: _authData,
+                    icono: Icons.lock_outline,
+                    labeltext: "Confirm password",
+                    type: 'password',
+                    enabledW: _authMode == AuthMode.Signup,
+                    validator: _authMode == AuthMode.Signup
+                        ? (value) {
+                            if (value != _passwordController.text) {
+                              return 'Passwords do not match!';
+                            }
+                          }
+                        : null,
+                    obscuretext: true,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            if (_isLoading)
+              CircularProgressIndicator()
+            else
+              RaisedButton(
+                child: Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+                onPressed: _submit,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).primaryTextTheme.button.color,
+              ),
+            FlatButton(
+              child: Text(
+                  '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+              onPressed: _switchAuthMode,
+              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              textColor: Theme.of(context).primaryColor,
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _TextfiedBuild extends StatelessWidget {
+  final Map<String, String> authData;
+  final Function validator;
+  final String type;
+  final String labeltext;
+  final IconData icono;
+  final TextInputType textInputType;
+  final TextEditingController textEditingController;
+  final bool obscuretext;
+  final bool enabledW;
+
+  const _TextfiedBuild(
+      {Key key,
+      @required this.authData,
+      @required this.validator,
+      @required this.type,
+      @required this.labeltext,
+      @required this.icono,
+      this.textInputType,
+      this.textEditingController,
+      this.obscuretext = false,
+      this.enabledW = true})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey[200]))),
+      child: TextFormField(
+        enabled: enabledW,
+        obscureText: obscuretext,
+        controller: textEditingController,
+        decoration: InputDecoration(
+            // suffixIcon: Icon(Icons.email),
+            icon: Icon(icono),
+            labelText: labeltext,
+            hintStyle: TextStyle(color: Colors.grey),
+            border: InputBorder.none),
+        keyboardType: textInputType,
+        validator: validator,
+        onSaved: (value) {
+          authData[type] = value;
+        },
       ),
     );
   }
